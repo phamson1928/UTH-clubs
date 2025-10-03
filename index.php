70c331bcaf23d70c1a5e9abd22c0386eac22fc93
sonpham1928 <pson4282@gmail.com>
2025-10-03 19:34:46 +0700

    Refactor index.php to include database configuration and initialize active section and club ID

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<?php include 'includes/header.php'; ?>
+<?php
+include 'config/database.php';
+// Determine initial section and selected club (for server-side rendering and JS bootstrapping)
+$activeSection = isset($_GET['section']) ? $_GET['section'] : 'home';
+$activeClubId = isset($_GET['club_id']) ? intval($_GET['club_id']) : null;
+include 'includes/header.php';
+?>
 
 <?php include 'pages/home.php'; ?>
 
